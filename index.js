3871de4811144149dadcb42d41b1d0bf9af2fe04
Christian Blaschke <mail@platdesign.de>
2015-02-04 04:15:18 +0100

    added feature to extend default schema/options for model definition

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 
 var passport = require('passport');
 var extend = require('extend');
-var path = require('path');
+
 
 var defaults = {
 	configId: 'account'
@@ -17,62 +17,99 @@ var configDefaults = {
 
 module.exports = function() {
 
-
+	var that = this;
 
 	if( this._config[defaults.configId] ) {
 		var config = extend(true, {}, configDefaults, this._config[defaults.configId]);
 
+
+		// config.dbService is definitely required!
 		if(!config.dbService) {
 			throw new Error('Missing dbService');
 		}
 
-		var dbGetter = new Function(config.dbService, 'return arguments[0];');
-
-
+		// service that returns the db-connection where account will be stored.
 		this.service('accountDB', function(inject) {
-			return inject(dbGetter);
+			return inject([config.dbService, function(db) {
+				return db;
+			}]);
 		});
 
-		var Model;
 
-		this.config(function(app, inject, accountDB) {
 
-			Model = accountDB.import( path.join(__dirname, 'lib', 'Account.js') );
+		/**
+		 * extendAccount service to be able to extend default model-schema/options
+		 *
+		 * @example
+		 * function(con, types) {
+		 * 	return {
+		 * 		schema: {
+		 * 			newAttr: types.STRING
+		 * 		},
+		 * 		options: {
+		 * 			classMethods:{
+		 * 				customLogin:function() { ... }
+		 * 			}
+		 * 		}
+		 * 	};
+		 * }
+		 */
+		var accountDefinerExtender = {};
+		this.service('extendAccount', function(accountDB) {
+			return function(handler) {
+				var result = handler(accountDB, accountDB.Sequelize);
+				extend(true, accountDefinerExtender, result);
+			};
+		});
+
+
+		// Configure passport and use passport middleware in app
+		this.config(function(app, accountDB) {
 
 			passport.serializeUser(function(user, done) {
 				done(null, user.id);
 			});
 
 			passport.deserializeUser(function(id, done) {
-				Model.findById(id).then(function(user){
+				accountDB.model('Account').findById(id)
+				.then(function(user){
 					done(null, user);
 				}, function(err) {
 					done(err);
 				});
 			});
 
+
+
+			that.logVerbose('Using passport');
 			app.use(passport.initialize());
+
+
+			that.logVerbose('Using passport-session');
 			app.use(passport.session());
 
+
+			that.logVerbose('Using active user in res.locals (account)');
 			app.use(function(req, res, next) {
-
-				/*
-				Object.prototype.__defineGetter__.apply(res.locals, ['account', function() {
-					return req.user;
-				}]);
-				*/
-
 				res.locals.account = req.user;
 				next();
 			});
 
+
 		});
 
 
+		// create basic routes and define model on db with extended schema/options
+		this.run(function(router, accountDB) {
 
-		this.run(function(router) {
+
+			/**
+			 * IDEA: Maybe it would be nice to be able to replace the default route handlers.
+			 * Or at least to be able to deactivate them. (Deactivation through config???)
+			 */
 
 
+			// Get basic information about account if authenticated
 			router.get( config.baseRoute, function(req, res, next) {
 				if(req.isAuthenticated()) {
 					res.json({
@@ -86,8 +123,11 @@ module.exports = function() {
 				}
 			});
 
+			// Login-Route
 			router.post( config.baseRoute + '/login', function(req, res, next) {
-				Model.login(req.body.email, req.body.password)
+
+				accountDB.model('Account').login(req.body.email, req.body.password)
+
 				.then(function(user) {
 					req.login(user, function(err) {
 						if (err) { return next(err); }
@@ -104,18 +144,24 @@ module.exports = function() {
 			});
 
 
+
+			// Logout-Route
 			router.post( config.baseRoute + '/logout', function(req, res) {
 				req.logout();
 				res.json(true);
 			});
 
+
+
+			// Register-Route
 			router.post( config.baseRoute + '/register', function(req, res, next) {
 
 				var username = req.body.username;
 				var email = req.body.email;
 				var password = req.body.password;
 
-				Model.register(username, email, password)
+				accountDB.model('Account').register(username, email, password)
+
 				.then(function(user) {
 					req.login(user, function(err) {
 						if (err) { return next(err); }
@@ -126,6 +172,23 @@ module.exports = function() {
 				});
 
 			});
+
+
+
+
+			// Read default account-definer Object
+			var accountDefiner = require('./lib/_Account.js')(accountDB.Sequelize);
+
+			// Extend it with the accountDefinerExtender-object
+			// A global object, which can be extended by extendAccount-service
+			extend(true, accountDefiner, accountDefinerExtender);
+
+			// Define model with schema and options from extended accountDefiner-object
+			var Model = accountDB.define('Account', accountDefiner.schema, accountDefiner.options);
+
+			// Sync model.
+			// To be shure that table in database exists when application starts: return the promise of sync function.
+			return Model.sync();
 		});
 
 	}
